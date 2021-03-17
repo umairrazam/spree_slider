6afa60ce124db715158226f487b800a87cc2d1dc
umairrazam <umairrazam@gmail.com>
2021-03-17 17:58:39 +0500

    fix(model): remove product relation

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,13 +1,13 @@
 module Spree
   module ProductDecorator
-    def self.prepended(base)
-      base.has_one :slide
-      base.after_destroy :destroy_slide_if_deleted
-    end
+    # def self.prepended(base)
+    #   base.has_one :slide
+    #   base.after_destroy :destroy_slide_if_deleted
+    # end
 
-    def destroy_slide_if_deleted
-      slide.update(published: false) if slide && deleted_at
-    end
+    # def destroy_slide_if_deleted
+    #   slide.update(published: false) if slide && deleted_at
+    # end
   end
 end
 
--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -3,18 +3,17 @@ class Spree::Slide < ActiveRecord::Base
                           class_name: 'Spree::SlideLocation',
                           join_table: 'spree_slide_slide_locations'
 
-  belongs_to :product, touch: true, optional: true
+  # belongs_to :product, touch: true, optional: true
 
   has_one_attached :image
 
-  validates :link_url, presence: true, url: true, unless: -> { product }
-  validates :name, :image, presence: true, unless: -> { product }
+  validates :name, :image, presence: true
   validates :image, content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
 
   scope :published, -> { where(published: true).order('position ASC') }
   scope :location, ->(location) { joins(:slide_locations).where('spree_slide_locations.name = ?', location) }
-  scope :product_slides, -> { published.where.not(product_id: nil).order('position ASC') }
-  scope :image_slides, -> { published.where(product_id: nil).order('position ASC') }
+  # scope :product_slides, -> { published.where.not(product_id: nil).order('position ASC') }
+  scope :image_slides, -> { published.order('position ASC') }
 
   STYLES = {
     preview: [120, 120],
@@ -27,15 +26,15 @@ class Spree::Slide < ActiveRecord::Base
   end
 
   def slide_name
-    name.blank? && product.present? ? product.name : name
+    name
   end
 
   def slide_link
-    link_url.blank? && product.present? ? product : link_url
+    link_url
   end
 
   def slide_image
-    !image.attached? && product.present? && product.images.any? ? product.images.first.attachment : image.attachment
+    image.attachment
   end
 
   # Helper for resizing
